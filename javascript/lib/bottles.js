f6aea964a7fc409ea0d30fb2cdcd31c424954e1d
Kevin R. Barnes <vinbarnes@gmail.com>
2021-03-16 20:24:56 -0500

    [js] get first js test passing

--- javascript/lib/bottles.js
+++ javascript/lib/bottles.js
@@ -1,3 +1,15 @@
 class Bottles {
+  // constructor() {
+
+  // }
+
+  verse(number) {
+    return '99 bottles of beer on the wall, ' +
+      '99 bottles of beer.\n' +
+      'Take one down and pass it around, ' +
+      '98 bottles of beer on the wall.\n';
+  }
 
 }
+
+export { Bottles };
