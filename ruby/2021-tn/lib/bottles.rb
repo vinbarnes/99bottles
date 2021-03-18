8088fd13aaf69820de61d0a20c3d941802f9f59c
Kevin R. Barnes <vinbarnes@gmail.com>
2021-03-18 06:59:15 -0500

    [rb] get another test passing

--- ruby/2021-tn/lib/bottles.rb
+++ ruby/2021-tn/lib/bottles.rb
@@ -1,7 +1,9 @@
 class Bottles
 
-  def verse(_)
-    "99 bottles of beer on the wall, 99 bottles of beer.\n" +
-    "Take one down and pass it around, 98 bottles of beer on the wall.\n"
+  def verse(number)
+    "#{number} bottles of beer on the wall, " +
+    "#{number} bottles of beer.\n" +
+    "Take one down and pass it around, " +
+    "#{number - 1} bottles of beer on the wall.\n"
   end
 end
