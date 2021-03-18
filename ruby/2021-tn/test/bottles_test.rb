6071971fa233c153a8d48b571b614d90fd030ed3
Kevin R. Barnes <vinbarnes@gmail.com>
2021-03-18 07:16:08 -0500

    [rb] get verse 1 test passing
    Convert to case statement for clarity

--- ruby/2021-tn/test/bottles_test.rb
+++ ruby/2021-tn/test/bottles_test.rb
@@ -34,4 +34,14 @@ class BottlesTest < Minitest::Test
 
     assert_equal expected, Bottles.new.verse(2)
   end
+
+  def test_verse_1
+    expected =
+      "1 bottle of beer on the wall, " +
+      "1 bottle of beer.\n" +
+      "Take it down and pass it around, " +
+      "No bottles of beer on the wall.\n"
+
+    assert_equal expected, Bottles.new.verse(1)
+  end
 end
