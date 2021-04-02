f371ec051227235e54e8488340481646ebc0f87f
Kevin R. Barnes <vinbarnes@gmail.com>
2021-04-02 02:17:11 -0500

    [rb] identify sameness between 1 and else cases

--- ruby/2021-tn/lib/bottles.rb
+++ ruby/2021-tn/lib/bottles.rb
@@ -8,8 +8,8 @@ class Bottles
       "Go to the store and buy some more, " +
       "99 bottles of beer on the wall.\n"
     when 1
-      "1 bottle of beer on the wall, " +
-      "1 bottle of beer.\n" +
+      "#{number} #{container(number)} of beer on the wall, " +
+      "#{number} #{container(number)} of beer.\n" +
       "Take it down and pass it around, " +
       "no more bottles of beer on the wall.\n"
     else
