f7088df13161ad9b1ad854e91f013bf1bd12fb69
Kevin R. Barnes <vinbarnes@gmail.com>
2021-04-02 08:54:27 -0500

    [rb] extract differences into the concept of action

--- ruby/2021-tn/lib/bottles.rb
+++ ruby/2021-tn/lib/bottles.rb
@@ -5,13 +5,21 @@ class Bottles
     when 0
       "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
       "#{quantity(number)} #{container(number)} of beer.\n" +
-      "Go to the store and buy some more, " +
+      "#{action(number)}, " +
       "99 bottles of beer on the wall.\n"
     else
       "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
       "#{quantity(number)} #{container(number)} of beer.\n" +
-      "Take #{pronoun(number)} down and pass it around, " +
+      "#{action(number)}, " +
       "#{quantity(number - 1)} #{container(number - 1)} of beer on the wall.\n"
+    end
+  end
+
+  def action(number=0)
+    if number == 0
+      "Go to the store and buy some more"
+    else
+      "Take #{pronoun(number)} down and pass it around"
     end
   end
 
