5b173520ab2ed0e853917e852707bae820cb94b9
Kevin R. Barnes <vinbarnes@gmail.com>
2021-04-02 08:49:07 -0500

    [rb] make the 2nd line the same in each case

--- ruby/2021-tn/lib/bottles.rb
+++ ruby/2021-tn/lib/bottles.rb
@@ -3,13 +3,13 @@ class Bottles
   def verse(number)
     case number
     when 0
-      "#{quantity(number).capitalize} bottles of beer on the wall, " +
-      "no more bottles of beer.\n" +
+      "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
+      "#{quantity(number)} #{container(number)} of beer.\n" +
       "Go to the store and buy some more, " +
       "99 bottles of beer on the wall.\n"
     else
       "#{quantity(number).capitalize} #{container(number)} of beer on the wall, " +
-      "#{number} #{container(number)} of beer.\n" +
+      "#{quantity(number)} #{container(number)} of beer.\n" +
       "Take #{pronoun(number)} down and pass it around, " +
       "#{quantity(number - 1)} #{container(number - 1)} of beer on the wall.\n"
     end
