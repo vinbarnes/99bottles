4c2ee3fc9dfb89b7ee1b66275fae0cdcedf60ad3
Kevin R. Barnes <vinbarnes@gmail.com>
2021-04-02 02:12:16 -0500

    [rb] remove the now redundant case 2 template

--- ruby/2021-tn/lib/bottles.rb
+++ ruby/2021-tn/lib/bottles.rb
@@ -12,11 +12,6 @@ class Bottles
       "1 bottle of beer.\n" +
       "Take it down and pass it around, " +
       "no more bottles of beer on the wall.\n"
-    when 2
-      "#{number} #{container(number)} of beer on the wall, " +
-      "#{number} #{container(number)} of beer.\n" +
-      "Take one down and pass it around, " +
-      "#{number - 1} #{container(number - 1)} of beer on the wall.\n"
     else
       "#{number} #{container(number)} of beer on the wall, " +
       "#{number} #{container(number)} of beer.\n" +
